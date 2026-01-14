db1b7c4fd16fc1ca006d0f495bf5985f577fbd57
s480049 <s480049@FXH-2998162754>
2026-01-14 09:23:33 -0500

    Furry Bunny

--- src/henryJavaCode/RSTProject.java
+++ src/henryJavaCode/RSTProject.java
@@ -11,17 +11,37 @@ public class RSTProject {
 		int userGuess = 0;
 		int guessCounter = 0; 
 		
-		Scanner input = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		// generate random number from 1 to 99
 		randomNum = (int)(Math.random() * 99) +1;
 		
 		// loop until user guesses right
-		while (userGuess != randomNum)  {        
+		while (userGuess != randomNum)  {  
 			
 			
+			// get users guess
+			System.out.println("Enter Guess from 1 to 99");
+			userGuess = sc.nextInt();
+			
+			guessCounter++;
+			
+			// check guess
+			if (userGuess == randomNum)  {
+				System.out.println("Correct!");
+				System.out.println("Number of guesses: " + guessCounter);
+				
+			}
+			else if  (userGuess > randomNum)  {
+				System.out.println("Too high!");
+			}
+			else {
+				System.out.println("Too low!");
+			}
 			
 		}
+		
+		sc.close();
 	}
 
 }
