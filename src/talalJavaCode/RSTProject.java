e9dc184a63d624700926ce37e7a2ce2bc24e836e
s500572 <s500572@FXH-9279188443>
2026-01-14 09:26:03 -0500

    I did my GUI and I am almost done the computer randomizer

--- src/talalJavaCode/RSTProject.java
+++ src/talalJavaCode/RSTProject.java
@@ -3,10 +3,22 @@ package talalJavaCode;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RSTProject {
 
 	private JFrame frame;
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
 
 	/**
 	 * Launch the application.
@@ -36,8 +48,82 @@ public class RSTProject {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setForeground(new Color(255, 255, 255));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ROCK,PAPER,SCISSORS");
+		lblNewLabel.setForeground(new Color(128, 0, 64));
+		lblNewLabel.setFont(new Font("Vineta BT", Font.PLAIN, 16));
+		lblNewLabel.setBounds(73, 28, 291, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField1 = new JTextField();
+		textField1.setForeground(new Color(128, 0, 64));
+		textField1.setBounds(56, 161, 86, 20);
+		frame.getContentPane().add(textField1);
+		textField1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter Your Choice");
+		lblNewLabel_1.setForeground(new Color(128, 0, 64));
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(57, 136, 98, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField2 = new JTextField();
+		textField2.setBounds(278, 161, 86, 20);
+		frame.getContentPane().add(textField2);
+		textField2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("AI Choice");
+		lblNewLabel_2.setForeground(new Color(128, 0, 64));
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(291, 136, 61, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JButton btnConfirmChoice = new JButton("Confirm Choice");
+		btnConfirmChoice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int userChoice = 0;
+				int aiChoice = 0;
+				
+				try {
+					
+					userChoice = Integer.parseInt(textField1.getText());
+				}
+				
+				catch (Exception e2) {
+					
+					JOptionPane.showMessageDialog(null, "Invalid Entry");
+				}
+				
+				
+				aiChoice = (int)Math.round(Math.random()*2+1);
+				textField2.setText(Integer.toString(aiChoice));
+					
+				
+				if (userChoice == aiChoice) {
+					
+					
+				textField3.setText("Tie!");
+			}
+			
+					
+			
+				
+				
+			}
+		});
+		btnConfirmChoice.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnConfirmChoice.setBounds(159, 202, 121, 23);
+		frame.getContentPane().add(btnConfirmChoice);
+		
+		textField3 = new JTextField();
+		textField3.setBounds(159, 88, 121, 20);
+		frame.getContentPane().add(textField3);
+		textField3.setColumns(10);
 	}
-
 }
