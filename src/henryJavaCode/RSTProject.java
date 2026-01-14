addab5c525691fa350d00c7d4645c18306db8bca
s480049 <s480049@FXH-2998162754>
2026-01-14 08:34:47 -0500

    Furry Bunny

--- src/henryJavaCode/RSTProject.java
+++ src/henryJavaCode/RSTProject.java
@@ -1,10 +1,27 @@
 package henryJavaCode;
+import java.util.Scanner;
 
 public class RSTProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		// variables
+		int randomNum = 0;
+		int userGuess = 0;
+		int guessCounter = 0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		// generate random number from 1 to 99
+		randomNum = (int)(Math.random() * 99) +1;
+		
+		// loop until user guesses right
+		while (userGuess != randomNum)  {
+			
+			
+			
+		}
 	}
 
 }
